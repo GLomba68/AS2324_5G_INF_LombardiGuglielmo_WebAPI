bbfdb40a1bd638d8793019541a93a3957dccde45
GLomba68 <guglielmo.lombardi@studenti.ittsrimini.edu.it>
2024-05-08 08:23:51 +0200

    Correzione errore

--- AS2324_5G_INF_LombardiGuglielmo_API/Controllers/MathController.cs
+++ AS2324_5G_INF_LombardiGuglielmo_API/Controllers/MathController.cs
@@ -13,13 +13,13 @@ namespace AS2324_5G_INF_LombardiGuglielmo_API.Controllers
         public JsonResult Getmultiplointero(int num, int factor)
         {
 
-            if(num % factor == 0)
+            if(factor % num == 0)
             {
                 return Json(new { output = $"Il numero inserito ({num}) è multiplo del secondo numero inserito({factor})", status = "OK" });
             }
             else
             {
-                return Json(new { output = $"Il numero inserito ({num})NON è multiplo del secondo numero inserito({factor})", status = "OK" });
+                return Json(new { output = $"Il numero inserito ({num}) NON è multiplo del secondo numero inserito ({factor})", status = "OK" });
             }
 
 
