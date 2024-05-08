0a366dcb90fb4de9bdcc286ab6a1163758b6b6ac
GLomba68 <guglielmo.lombardi@studenti.ittsrimini.edu.it>
2024-05-08 08:46:20 +0200

    Implementazione "Getpotenza"

--- AS2324_5G_INF_LombardiGuglielmo_API/Controllers/MathController.cs
+++ AS2324_5G_INF_LombardiGuglielmo_API/Controllers/MathController.cs
@@ -32,6 +32,17 @@ namespace AS2324_5G_INF_LombardiGuglielmo_API.Controllers
         {
             return Json(new { output = $"Il valore dell' ipotenusa è : " + Math.Sqrt((cateto1 * cateto1) + (cateto2 * cateto2)) , status = "OK" });
         }
+
+        [HttpGet("Getpotenza")]
+        public JsonResult Getpotenza(int b, int esponente)
+        {
+            int risultato = b;
+            for (int i = 1; i<esponente;i++)
+            {
+                risultato *= b;
+            }
+            return Json(new { output = $"Il valore della potenza : {b}^{esponente} è = {risultato}", status = "OK" });
+        }
     }
 }
 
