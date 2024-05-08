253e7b4c4d8bc5fc93292ee5a73a68f565856f4d
GLomba68 <guglielmo.lombardi@studenti.ittsrimini.edu.it>
2024-05-08 08:07:51 +0200

    Aggiungere i file di progetto.

---
+++ AS2324_5G_INF_LombardiGuglielmo_API/Controllers/MathController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS2324_5G_INF_LombardiGuglielmo_API.Controllers
+{
+    public class MathController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        
+
+
+
+
+        }
+    }
+}
