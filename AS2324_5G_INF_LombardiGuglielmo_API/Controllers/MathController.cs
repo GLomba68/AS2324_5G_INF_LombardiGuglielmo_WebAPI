7a9b7eca4568073fb7887436e0dec27d682e9061
GLomba68 <guglielmo.lombardi@studenti.ittsrimini.edu.it>
2024-05-08 08:17:02 +0200

    Sviluppo GetMultiplo e ritorno Json

--- AS2324_5G_INF_LombardiGuglielmo_API/Controllers/MathController.cs
+++ AS2324_5G_INF_LombardiGuglielmo_API/Controllers/MathController.cs
@@ -13,7 +13,14 @@ namespace AS2324_5G_INF_LombardiGuglielmo_API.Controllers
         public JsonResult GetMultiplo(int num, int factor)
         {
 
-            
+            if(num % factor == 0)
+            {
+                return Json(new { output = $"Il numero inserito ({num}) è multiplo del secondo numero inserito({factor})", status = "OK" });
+            }
+            else
+            {
+                return Json(new { output = $"Il numero inserito ({num})NON è multiplo del secondo numero inserito({factor})", status = "OK" });
+            }
 
 
         }
